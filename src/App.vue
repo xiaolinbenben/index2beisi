b3267c62c31510987d3b5ff1f6c2d5068b0671cf
Burtry <121756403+Burtry@users.noreply.github.com>
2025-02-17 22:11:08 +0800

    页面Init

---
+++ src/App.vue
@@ -0,0 +1,135 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+        <div class="logo">
+          <img src="./assets/img/logo.png" alt />
+        </div>
+        <el-menu :default-active="defaultActive" class="el-menu-demo" mode="horizontal" @select="handleSelect"
+          :router="router">
+          <el-menu-item index="/">首页</el-menu-item>
+          <el-menu-item index="/download">下载APP</el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      router: true,
+      defaultActive: "/",
+      isShow: false
+    };
+  },
+  methods: {
+    handleSelect(key) {
+      this.isShow = this.defaultActive != key;
+      window.console.log(this.isShow);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+}
+
+html,
+body {
+  height: 100%;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+.el-header {
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  width: 1240px;
+  margin: 0 auto;
+  //background-color: #fff;
+
+  .logo {
+    width: 240px;
+    padding: 10px;
+
+    img {
+      width: 100%;
+      line-height: 60px;
+    }
+  }
+}
+
+.el-main {
+  padding: 0 !important;
+}
+
+.footer {
+  width: 100%;
+  height: 216px;
+  overflow: hidden;
+  background-color: #ffd700;
+
+  &-content {
+    width: 1240px;
+    margin: 0 auto;
+    padding-top: 20px;
+    display: flex;
+    justify-content: space-between;
+
+    .content-nav {
+      display: flex;
+      justify-content: space-around;
+
+      li {
+        display: flex;
+        flex-direction: column;
+        padding: 0 20px;
+        //justify-content: center;
+        align-items: flex-start;
+
+        p {
+          font-size: 20px;
+          color: #d4edff;
+          padding: 10px 0;
+        }
+
+        span {
+          color: #f7f7f7;
+          font-weight: 300;
+          padding: 5px 0;
+        }
+      }
+    }
+
+    img {
+      width: 170px;
+      height: 170px;
+      padding: 10px;
+    }
+  }
+
+  .copyright {
+    height: 30px;
+    background: #adaea2;
+
+    span {
+      color: #fff;
+      line-height: 30px;
+    }
+  }
+}
+</style>
---
+++ src/router.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+let router = new Router({
+  routes: [{
+    path: '/',
+    name: 'home',
+    component: () => import('./views/Home.vue')
+  },
+  {
+    path: '/download',
+    name: 'download',
+    component: () => import('./views/Download.vue')
+  }
+  ]
+})
+
+export default router
