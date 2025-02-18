0b1dea49b3ad994467c1419eafc10ce546b9b431
Burtry <121756403+Burtry@users.noreply.github.com>
2025-02-18 13:26:21 +0800

    添加产品介绍

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,13 +7,31 @@
           <p>FuZhou Beisi</p>
         </div>
       </swiper-slide>
-
       <swiper-slide class="swiper-slide slide-two">
-        <div class="page">
+        <div class="product-intro">
           <h3>产品介绍</h3>
-          <!-- 产品介绍 -->
+          <p>
+            “黄色仓库”不仅是一款便捷的APP，更是一个集H5技术应用、开放生态、友好体验于一体的创新平台。它如同一座桥梁，紧密连接着用户与开发者，致力于打破传统应用市场的壁垒。
+          </p>
+          <p>
+            在“黄色仓库”，用户可以告别手机APP占用内存过大、广告弹窗频繁、功能复杂冗余的困扰，享受更加纯净、高效的使用体验。同时，开发者也能摆脱严格的应用审核要求和高昂的上架成本，在一个更加自由、开放的环境中释放创意，实现价值。
+          </p>
+          <p>
+            “黄色仓库”的诞生，不仅是对当前软件市场痛点的有力回应，更是对未来应用生态的积极探索。它凭借独特的创新理念和无限的潜力，开启了H5应用新时代，为软件行业的可持续发展注入新的活力和动力。
+          </p>
+          <div class="logo-container">
+            <img src="../assets/img/logo_2.png" alt="黄色仓库 Logo" class="logo" />
+            <!-- 下载按钮 -->
+            <div class="download-links">
+              <router-link to="/download" class="btn download-btn">
+                <img src="../assets/img/sanjiao.png" alt="下载图标" /> 前往下载
+              </router-link>
+            </div>
+
+          </div>
         </div>
       </swiper-slide>
+
     </swiper>
   </div>
 </template>
@@ -122,6 +140,63 @@ export default {
   background: linear-gradient(135deg, #fff8dc, #ffd700);
   color: #333;
   box-sizing: border-box;
+}
+
+
+.product-intro {
+  padding: 30px;
+  color: #333;
+  text-align: left;
+
+  h3 {
+    font-size: 32px;
+    margin-bottom: 20px;
+    color: #222;
+  }
+
+  p {
+    font-size: 18px;
+    line-height: 1.6;
+    margin-bottom: 15px;
+  }
+
+  .logo-container {
+    text-align: center;
+    margin-top: 30px;
+
+    .logo {
+      width: 450px;
+      /* 根据你的需求调整宽度 */
+      height: auto;
+    }
+  }
+
+  .download-links {
+    margin: 20px 0;
+
+    .download-btn {
+      display: inline-flex;
+      align-items: center;
+      gap: 12px;
+      padding: 15px 30px;
+      background-color: #ffcc00;
+      border-radius: 12px;
+      text-decoration: none;
+      color: #000;
+      font-weight: bold;
+      box-shadow: 0 6px 15px rgba(0, 0, 0, 0.2);
+      transition: transform 0.2s;
+
+      &:hover {
+        transform: translateY(-5px);
+      }
+
+      img {
+        width: 28px;
+        height: 28px;
+      }
+    }
+  }
 }
 
 .order {
